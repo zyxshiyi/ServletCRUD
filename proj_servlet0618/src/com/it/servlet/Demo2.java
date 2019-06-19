cb42bcd3ca67782c4120b5d01e70bd7b6603d4bb
zyxshiyi <zyx80002@126.com>
2019-06-19 17:54:16 +0800

    第二次提交

---
+++ proj_servlet0618/src/com/it/servlet/Demo2.java
@@ -0,0 +1,10 @@
+package com.it.servlet;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("----demo2.git--");
+	}
+
+}
